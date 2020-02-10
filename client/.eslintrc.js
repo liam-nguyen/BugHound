a70ab462e0b64566417b50a462a02fe78627c8f9
junlico <sean.junlico@gmail.com>
2020-02-09 18:12:06 -0800

    .eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: { jsx: true },
+    sourceType: 'module',
+  },
+  env: { browser: true, es6: true },
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      node: { extensions: ['.js', '.jsx'], paths: ['src'] },
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'no-console': 'off',
+  },
+};
