2ce49e7a6e2f24591f8abd11d37162fad9033877
junlico <sean.junlico@gmail.com>
2020-02-09 22:36:01 -0800

    rm .ts

--- client/src/state/rootReducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import counterReducer from '../features/counter'
-
-const rootReducer = combineReducers({ count: counterReducer })
-
-export default rootReducer
