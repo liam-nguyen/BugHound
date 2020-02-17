3161678081f982c0e2e122cbe758690407834268
Mcurci95 <mcurci95@gmail.com>
2020-02-16 18:07:57 -0800

    Registered some models for admin access

--- issues/admin.py
+++ issues/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here.\
+from .models import Issue, Status, BugType, Resolution
+
+admin.site.register(Issue)
+admin.site.register(Status)
+admin.site.register(BugType)
+admin.site.register(Resolution)
+
