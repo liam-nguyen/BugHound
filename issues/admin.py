004fc15decaedf1741b0abd4c41d1d1f30734431
Mcurci95 <mcurci95@gmail.com>
2020-02-23 17:53:31 -0800

    Adding models to admin site

--- issues/admin.py
+++ issues/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.\
-from .models import Issue, Status, BugType, Resolution
+from .models import Issue, Status, BugType, Resolution, Priority, FunctionalArea, Severity
 
 admin.site.register(Issue)
 admin.site.register(Status)
 admin.site.register(BugType)
 admin.site.register(Resolution)
+admin.site.register(Severity)
+admin.site.register(Priority)
 
