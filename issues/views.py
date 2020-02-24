ccaa3a779af1d1ba4ac2543dc6911f6a76a1ca99
Mcurci95 <mcurci95@gmail.com>
2020-02-23 20:50:54 -0800

    Issue view

--- issues/views.py
+++ issues/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Issue
 
 def index(request):
     return HttpResponse("Welcome to BugHound!")
+
+def issue(request, issueID):
+    try:
+        issue = Issue.objects.get(pk=issueID)
+    except Issue.DoesNotExist:
+        raise HTTP404("Issue does not exist")
+    return issue
