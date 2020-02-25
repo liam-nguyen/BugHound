df398dc55a3a5d070b40d33273d8a29e9f24a273
Mcurci95 <mcurci95@gmail.com>
2020-02-24 21:07:54 -0800

    Using JSON Serializer

--- issues/views.py
+++ issues/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Issue
+from django.core import serializers
 
 def index(request):
     return HttpResponse("Welcome to BugHound!")
@@ -8,11 +9,15 @@ def index(request):
 def issue(request, issueID):
     try:
         issue = Issue.objects.get(pk=issueID)
-        issueObj = {
-            'ProgramID': str(issue.programID),
-            'BugTypeID': str(issue.bugtypeID)
-        }
-        print(issueObj)
+        # issueObj = {
+        #     'ProgramID': str(issue.programID),
+        #     'BugTypeID': str(issue.bugtypeID),
+        #     'SeverityID': str(issue.severityID),
+        #     'ReportedByID': str(issue.reportedByID)
+
+        # }
+        data = serializers.serialize("json", [issue])
+        print(data)
     except Issue.DoesNotExist:
         raise Http404("Issue does not exist")
-    return JsonResponse(issueObj)
+    return JsonResponse(data, safe=False)
