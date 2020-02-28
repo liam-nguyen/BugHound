6ec01de8edca1c1d8fc1ce21772a138e31343877
Mcurci95 <mcurci95@gmail.com>
2020-02-28 13:00:51 -0800

    Program Form

--- issues/forms.py
+++ issues/forms.py
@@ -1,6 +1,11 @@
 from django import forms
-from .models import Program
+from .models import Program, Version
 
 class AreaForm(forms.Form):
     program = forms.ModelChoiceField(queryset=Program.objects.all())
+    name = forms.CharField()
+
+class ProgramForm(forms.Form):
+    version = forms.ModelChoiceField(queryset=Version.objects.all())
+    release = forms.IntegerField()
     name = forms.CharField()
