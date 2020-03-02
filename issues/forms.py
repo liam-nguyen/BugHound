c7568792b420cfe776c15046f5405ca160fb9fdf
Mcurci95 <mcurci95@gmail.com>
2020-03-01 17:40:31 -0800

    Issue Search Form

--- issues/forms.py
+++ issues/forms.py
@@ -1,15 +1,31 @@
 from django import forms
-from .models import Program, Version, Department
+from .models import Program, Version, Department, BugType, Severity, FunctionalArea, Employee, Status, Priority, Resolution
 
+# Issue Forms
+class IssueForm(forms.Form):
+    program = forms.ModelChoiceField(queryset=Program.objects.all(), required=False)
+    bugType = forms.ModelChoiceField(queryset=BugType.objects.all(), required=False)
+    severirty = forms.ModelChoiceField(queryset=Severity.objects.all(), required=False)
+    functionalArea = forms.ModelChoiceField(queryset=FunctionalArea.objects.all(), required=False)
+    assignedTo = forms.ModelChoiceField(queryset=Employee.objects.all(), required=False)
+    reportedBY = forms.ModelChoiceField(queryset=Employee.objects.all(), required=False)
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=False)
+    priroty = forms.ModelChoiceField(queryset=Priority.objects.all(), required=False)
+    resolution = forms.ModelChoiceField(queryset=Resolution.objects.all(), required=False)
+
+
+# Area Forms
 class AreaForm(forms.Form):
     program = forms.ModelChoiceField(queryset=Program.objects.all())
     name = forms.CharField()
 
+# Program Forms
 class ProgramForm(forms.Form):
     version = forms.ModelChoiceField(queryset=Version.objects.all())
     release = forms.IntegerField()
     name = forms.CharField()
 
+# Employee Forms
 class EmployeeForm(forms.Form):
     levelChoices = (
         (1, '1'),
