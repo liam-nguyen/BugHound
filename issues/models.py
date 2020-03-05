9f26ce042c3146662990af7b337373a6f130086a
Mcurci95 <mcurci95@gmail.com>
2020-03-04 18:42:51 -0800

    Issue model

--- issues/models.py
+++ issues/models.py
@@ -82,7 +82,7 @@ class Program(models.Model):
 # FunctionalArea
 class FunctionalArea(models.Model):
     programID = models.ForeignKey(Program, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
 
