fd859e10bc6b062f1e6cb1a014f6fec2aa7a01e1
liam-nguyen <nguyen.dch@gmail.com>
2020-04-29 04:05:57 -0700

    Restructure program update view

--- issues/templatetags/issues_extras.py
+++ issues/templatetags/issues_extras.py
@@ -5,3 +5,34 @@ register = template.Library()
 @register.filter
 def get_attr(obj, key):
     return getattr(obj, key)
+
+
+@register.filter
+def partition(thelist, n):
+    """
+    Break a list into ``n`` pieces. The last list may be larger than the rest if
+    the list doesn't break cleanly. That is::
+
+        >>> l = range(10)
+
+        >>> partition(l, 2)
+        [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
+
+        >>> partition(l, 3)
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8, 9]]
+
+        >>> partition(l, 4)
+        [[0, 1], [2, 3], [4, 5], [6, 7, 8, 9]]
+
+        >>> partition(l, 5)
+        [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
+
+    """
+    print(thelist)
+    try:
+        n = int(n)
+        thelist = list(thelist)
+    except (ValueError, TypeError):
+        return [thelist]
+    p = len(thelist) / n
+    return [thelist[p*i:p*(i+1)] for i in range(n - 1)] + [thelist[p*(i+1):]]
